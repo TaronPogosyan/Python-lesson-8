820dd7b2b3cd2373d86fe378b3ace04f6d0df2ba
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-10-22 22:31:12 +0300

    Задача 8 решено

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 def write_database(a):
-    with open('seminar8/database.txt', 'a', encoding='utf=8') as f:
+    with open('Python DZ 8/database.txt', 'a', encoding='utf=8') as f:
         f.write(f'{a}\n')
 
 def read_database():
     mass = []
-    with open('seminar8/database.txt', 'r', encoding='utf=8') as f:
+    with open('Python DZ 8/database.txt', 'r', encoding='utf=8') as f:
         for i in f:
             mass.append(i)
 
@@ -12,12 +12,12 @@ def read_database():
 
 
 def rename_data(mass):
-    with open('seminar8/database.txt', 'w', encoding = 'utf=8') as f:
+    with open('Python DZ 8/database.txt', 'w', encoding = 'utf=8') as f:
         for i in mass:
             f.write(i)
 
 def write_re(a):
-    with open('seminar8/database.txt', 'w', encoding='utf=8') as f:
+    with open('Python DZ 8/database.txt', 'w', encoding='utf=8') as f:
         for i in a:
             f.write(i+'\n')
         
