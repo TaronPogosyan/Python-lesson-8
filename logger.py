706e3fb4de42a2b6ad13418fdbe07608aacc6598
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-10-22 22:39:19 +0300

    Проверяем задачу 2

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 def write_database(a):
-    with open('Python DZ 8/database.txt', 'a', encoding='utf=8') as f:
+    with open('database.txt', 'a', encoding='utf=8') as f:
         f.write(f'{a}\n')
 
 def read_database():
     mass = []
-    with open('Python DZ 8/database.txt', 'r', encoding='utf=8') as f:
+    with open('database.txt', 'r', encoding='utf=8') as f:
         for i in f:
             mass.append(i)
 
@@ -12,12 +12,12 @@ def read_database():
 
 
 def rename_data(mass):
-    with open('Python DZ 8/database.txt', 'w', encoding = 'utf=8') as f:
+    with open('database.txt', 'w', encoding = 'utf=8') as f:
         for i in mass:
             f.write(i)
 
 def write_re(a):
-    with open('Python DZ 8/database.txt', 'w', encoding='utf=8') as f:
+    with open('database.txt', 'w', encoding='utf=8') as f:
         for i in a:
             f.write(i+'\n')
         
